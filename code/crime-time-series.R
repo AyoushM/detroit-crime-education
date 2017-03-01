d04a7e15d67733f587fcd46e09c1f5b648d884ea
vibes <vaibhavi.rangarajan@gmail.com>
2017-03-01 15:51:09 -0800

    stub files to clean up education, and create merged dataframe for crime

---
+++ code/crime-time-series.R
@@ -0,0 +1,23 @@
+library(acs)
+library(geo)
+library(dplyr)
+library(plyr)
+library(ggplot2)
+library(lubridate)
+library(reshape) 
+library(data.table)
+
+library(astsa)
+
+
+crime <- read.csv("crimewithzips.csv")
+summary(crime)
+colnames(crime)
+
+#When are crimes most likely
+
+
+#Where do crimes occur
+
+
+#Machine Learning
